aa3c9f419717533737cd3839a93c4552843eaaa9
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-05-13 15:48:44 -0300

    Telegram integration

--- checkHashes.py
+++ checkHashes.py
@@ -2,6 +2,9 @@ from argparse import ArgumentParser
 from components.directoryHash import GetHashofDirs
 from components.fileHash import GetFileHash
 import json
+import requests
+from dotenv import load_dotenv
+import os
 
 if __name__ == "__main__":
     # parser = ArgumentParser(description=MSG_DESCRIPTION)
@@ -11,10 +14,15 @@ if __name__ == "__main__":
 
     # args = parser.parse_args()
 
+    load_dotenv(verbose=True)
+    token = os.getenv("TOKEN")
+    chat_id = os.getenv("CHAT_ID")
+    url_base = f"https://api.telegram.org/bot{token}/"
+
     with open("output.json", "r") as f:
         data = json.load(f)
 
-    status = True
+    error = []
 
     for i in data:
         try:
@@ -23,8 +31,13 @@ if __name__ == "__main__":
             result = GetFileHash(i)
 
         if data[i] != result:
-            status = False
-            print(f"Diferente {i}")
-        else:
-            status = False
-            print(f"Passou {i}")
+            error += [i]
+
+    resp = "Os seguintes arquivos ou diretórios foram modificados:\n\n"
+
+    if len(error) > 0:
+        for k, i in enumerate(error):
+            resp += f"{k+1}. " + i + "\n"
+
+        link_requisicao = f"{url_base}sendMessage?chat_id={chat_id}&text={resp}"
+        requests.get(link_requisicao)
